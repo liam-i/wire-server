a5172c58ee08d364b8f2ed9d69cafed1e8ebf156
Stefan Matting <stefan@wire.com>
2021-02-25 16:15:20 +0100

    use new types

--- libs/brig-types/src/Brig/Types/SparAuthId.hs
+++ libs/brig-types/src/Brig/Types/SparAuthId.hs
@@ -1,11 +1,11 @@
 module Brig.Types.SparAuthId
   ( AuthId (..),
-    AuthPassDetails (..),
     EmailSource (..),
+    EmailWithSource (..),
+    ScimDetails (..),
     ExternalId (..),
     runAuthId,
     authIdUref,
-    authIdEmail,
   )
 where
 
@@ -17,11 +17,12 @@ import Wire.API.User.Identity
 
 data AuthId
   = AuthSAML SAML.UserRef
-  | AuthPass AuthPassDetails
-  | AuthBoth SAML.UserRef AuthPassDetails
+  | AuthSCIM ScimDetails
+  | AuthBoth TeamId SAML.UserRef (Maybe EmailWithSource) -- userref and externalid are iso. Both DB indices (SAMLUserRef index and ExternalId index) are kept in sync
   deriving (Eq, Show)
 
-data AuthPassDetails = AuthPassDetails ExternalId Email EmailSource
+data EmailWithSource
+  = EmailWithSource Email EmailSource
   deriving (Eq, Show)
 
 data EmailSource
@@ -33,25 +34,51 @@ data ExternalId
   = ExternalId TeamId Text
   deriving (Eq, Show)
 
--- | Take apart a 'Auth', using 'SAML.UserRef' if available, otehrwise 'Email'.
+data ScimDetails = ScimDetails ExternalId EmailWithSource
+  deriving (Eq, Show)
+
+-- | Take apart a 'AuthId', using 'SAML.UserRef' if available, otherwise 'Email'.
 runAuthId :: (SAML.UserRef -> a) -> (Email -> a) -> AuthId -> a
 runAuthId doUref doEmail = \case
   AuthSAML uref -> doUref uref
-  AuthPass (AuthPassDetails _ email _) -> doEmail email
-  AuthBoth uref _ -> doUref uref
+  AuthSCIM (ScimDetails _ (EmailWithSource email _)) -> doEmail email
+  AuthBoth _tid uref _ -> doUref uref
+
+-- samlToExtId :: TeamId -> SAML.UserRef -> ExternalId
+-- samlToExtId = undefined
+
+-- getScimDetails :: AuthId -> Maybe ScimDetails
+-- getScimDetails = error "Brig.Types.SparAuthId.getScimDetails"
+
+-- getScimEmail :: AuthId -> Maybe Email
+-- getScimEmail (AuthSAML _) = Nothing
+-- getScimEmail (AuthSCIM _) = Nothing
+
+-- getScimDetails (AuthSAML _uref) = Nothing
+-- getScimDetails (AuthSCIM d) = Just d
+-- getScimDetails (AuthBoth _tid _uref Nothing) = Nothing
+-- getScimDetails (AuthBoth tid uref (Just ews)) = Just $ ScimDetails (samlToExtId tid uref) ews
+
+-- setScimDetails :: ScimDetails -> AuthId -> AuthId
+-- setScimDetails d (AuthSAML uref) = AuthSAML uref
+-- setScimDetails d (AuthSCIM _) = AuthSCIM d
+-- setScimDetails (ScimDetails extId (Just ews)) (AuthBoth tid uref mbEmailSource) = error "TODO"
+-- setScimDetails (ScimDetails extId Nothing) (AuthBoth tid uref mbEmailSource) = error "TODO"
 
 authIdUref :: Prism' AuthId SAML.UserRef
 authIdUref = prism' AuthSAML $
   \case
     AuthSAML uref -> Just uref
-    AuthPass (AuthPassDetails _ _ _) -> Nothing
-    AuthBoth uref _ -> Just uref
+    AuthSCIM _ -> Nothing
+    AuthBoth _ uref _ -> Just uref
 
-authIdEmail :: AuthId -> Maybe Email
-authIdEmail =
-  \case
-    AuthSAML _ -> Nothing
-    AuthPass (AuthPassDetails _ email _) -> Just email
-    AuthBoth _ (AuthPassDetails _ email _) -> Just email
-
-makeLenses ''AuthId
+-- authIdEmail :: AuthId -> Maybe Email
+-- authIdEmail =
+--   \case
+--     AuthSAML _ -> Nothing
+--     AuthSCIM (ScimDetails _ mbes) -> emailFromSource mbes
+--     AuthBoth _ uref mbes) -> Just email
+--   where
+--     emailFromSource :: Maybe EmailWithSource -> Maybe Email
+--     emailFromSource (Just (EmailWithSource email _)) = Just email
+--     emailFromSource Nothing = Nothing
